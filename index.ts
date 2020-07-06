ceba8f01ba043db5f4ab04bbd75137359cbeda36
purplef1sh <vrh.416@gmail.com>
2020-07-05 22:27:08 -0500

    1 file modified

--- index.ts
+++ index.ts
@@ -6,12 +6,46 @@ const titleEl: HTMLElement = document.querySelector('.title');
 
 const strengthList = [
   'Narrow-Squat.jpg',
-  'Around-The-World-Plank.jpg',
-  'Basic-Push-Up.jpg',
   'Basic-Squat.jpg',
+  'Wide-Squat.jpg',
+  'Lunge.jpg',
+  'Lunge.jpg',
+  'Renegade-Row.jpg',
+  'Renegade-Row.jpg',
+  'Basic-Push-Up.jpg',
   'Bicycle-Crunch.jpg',
-  'Body-Saw.jpg'
+  'Table-Top-Heel-Tap.jpg',
+  'Leg-Lifts-Hold.png',
+  'Wall-Plank-Pelvic-Tilt.jpeg',
+  'Pelvic-Bridge.jpg',
+  'High-Plank.jpeg',
+  'Plankle-Touch.png',
+  'Wide-Plank.jpeg',
+  'Regular-Plank.jpeg',
+  'Around-The-World-Plank.jpg',
+  'Around-The-World-Plank.jpg',
+  'Body-Saw.jpg', 
+  'Wall-Sitjpg'
+];
+const stretchList = [
+  'Doorway-Arm-Stretch.jpg',
+  'Doorway-Arm-Stretch.jpg',
+  'Downward-Dog.jpg',
+  'Side-Split.jpeg',
+  'Side-Split.jpeg',
+  'Hip-Flexor.jpg',
+  'Hip-Flexor.jpg',
+  'Seated-Spinal-Twist.jpg',
+  'Seated-Spinal-Twist.jpg',
+  'Standing-Side-Reach.jpg',
+  'Standing-Side-Reach.jpg',
+  'Pigeon-Pose.jpeg',
+  'Neck-Rolls.jpg',
+  'Downward-Dog-Hand-Up.jpg',
+  'Downward-Dog-Hand-Up.jpg',
+
 ]
+
 
 function getImageUrl(imageName: string): string {
   return `https://github.com/purplef1sh/siit/blob/master/images/${imageName}?raw=true`;
@@ -23,7 +57,8 @@ function getNameFromImageName(imageName: string): string {
 
 
 // TODO: Create function to display(imageName)
-const imageName = imageNamesList[2];
+const imageName = stretchList[5];
+
 
 
 imageEl.src = getImageUrl(imageName);
